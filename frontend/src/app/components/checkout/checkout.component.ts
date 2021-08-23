27e16dd56aaff3012596e990afc419a68139c5e3
shreyas710 <shreyasnk5321@gmail.com>
2021-08-23 17:03:05 +0530

    Finishing Cart, Checkout and ThankYou Component

--- frontend/src/app/components/checkout/checkout.component.ts
+++ frontend/src/app/components/checkout/checkout.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { CartModelServer } from 'src/app/models/cart.model';
+import { CartService } from 'src/app/services/cart.service';
+import { OrderService } from 'src/app/services/order.service';
 
 @Component({
   selector: 'app-checkout',
@@ -6,10 +12,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./checkout.component.scss']
 })
 export class CheckoutComponent implements OnInit {
+  cartData!: CartModelServer;
+  cartTotal!: number;
 
-  constructor() { }
+  constructor(private cartService: CartService, private orderService: OrderService, private router: Router, private spinner: NgxSpinnerService) { }
+
+  // this.checkoutForm = this.fb.group({
+  //   firstname: ['', [Validators.required]],
+  //   lastname: ['', [Validators.required]],
+  //   email: ['', [Validators.required, Validators.email]],
+  //   phone: ['', [Validators.required]],
+
+  // });
 
   ngOnInit(): void {
+    this.cartService.cartDataObs$.subscribe(data => this.cartData = data);
+    this.cartService.cartTotal$.subscribe(total => this.cartTotal = total);
   }
 
+  onCheckout() {
+    this.spinner.show().then(p => {
+      this.cartService.CheckoutFromCart(1);
+    })
+  };
 }
--- frontend/src/app/components/thankyou/thankyou.component.ts
+++ frontend/src/app/components/thankyou/thankyou.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { OrderService } from 'src/app/services/order.service';
 
 @Component({
   selector: 'app-thankyou',
@@ -6,10 +8,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./thankyou.component.scss']
 })
 export class ThankyouComponent implements OnInit {
+  message: string;
+  orderId: number;
+  products;
+  cartTotal;
 
-  constructor() { }
+  constructor(private router: Router,
+    private orderService: OrderService) {
 
-  ngOnInit(): void {
+    const navigation = this.router.getCurrentNavigation();
+    const state = navigation!.extras.state as {
+      message: string,
+      products: ProductResponseModel[],
+      orderId: number,
+      total: number
+    };
+
+    this.message = state.message;
+    this.orderId = state.orderId;
+    this.products = state.products;
+    this.cartTotal = state.total;
   }
 
+  ngOnInit() {
+
+  }
+}
+
+interface ProductResponseModel {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  image: string;
+  quantityOrdered: number;
 }
